85fd4abe32cd5605ebf0c4bfae46f0d15fdfd3c3
Adam Oberle <oberle.adam@gmail.com>
2023-06-25 18:17:17 -0500

    added sql queries for table creation and grape table population in database.sql file

--- database.sql
+++ database.sql
@@ -8,3 +8,39 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+-- Creates wine table in database
+CREATE TABLE "wine" (
+	"id" SERIAL PRIMARY KEY,
+	"vineyard" VARCHAR (255) NOT NULL,
+	"vintage" INTEGER NOT NULL,
+	"grape_id" INTEGER NOT NULL REFERENCES "grape",
+	"price" INTEGER,
+	"place_bought" VARCHAR (255),
+	"notes" VARCHAR (255),
+	"rating" INTEGER,
+	"user_id" INTEGER NOT NULL REFERENCES "user"
+);
+
+-- Creates grape table in database
+CREATE TABLE "grape" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (50)
+);
+
+-- Populates grape table in database
+INSERT INTO "grape" ("name") VALUES 
+	('Cabernet Sauvignon'),
+	('Merlot'),
+	('Pinot Noir'),
+	('Syrah'),
+	('Malbec'),
+	('Zinfandel'),
+	('Chardonnay'),
+	('Riesling'),
+	('Pinot Grigio'),
+	('Sauvignon Blanc'),
+	('Chenin Blanc'),
+	('Moscato');
+	
+	
