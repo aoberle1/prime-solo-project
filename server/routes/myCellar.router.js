c5b60cf64268453ee43a3d5a9161577d3536dacf
Adam Oberle <oberle.adam@gmail.com>
2023-06-26 14:43:35 -0500

    added DELETE button functionality to DELETE buttons on My Cellar page

--- server/routes/myCellar.router.js
+++ server/routes/myCellar.router.js
@@ -6,22 +6,23 @@ const {
 } = require('../modules/authentication-middleware');
 
 router.get('/', rejectUnauthenticated, (req, res) => {
-    const queryText = `SELECT "wine"."id", "vineyard", "vintage", "grape"."name", "price", "place_bought", "notes", "rating" FROM "wine"
+    const queryText = `SELECT "wine"."id", "vineyard", "vintage", "grape"."name", "price", "place_bought", "notes", "rating" 
+    FROM "wine"
     JOIN "user" ON "user"."id" = "wine"."user_id"
     JOIN "grape" ON "grape"."id" = "wine"."grape_id"
     WHERE "user_id" = $1;`;
     console.log('req.user.id in cellar GET:', req.user.id)
     pool.query(queryText, [req.user.id])
-    .then(result => {
-        res.send(result.rows);
-    })
-    .catch(error => {
-        console.log('error in initial GET for myCellar:', error);
-        res.sendStatus(500);
-    })
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('error in initial GET for myCellar:', error);
+            res.sendStatus(500);
+        })
 });
 
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
     console.log('req.body for POST is:', req.body);
     const dataPackage = [
         req.body.vineyard,
@@ -39,12 +40,27 @@ router.post('/', (req, res) => {
     ("vineyard", "vintage", "grape_id", "price", "place_bought", "notes", "rating", "user_id") 
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
     pool.query(queryText, dataPackage)
-    .then(result => {
-        res.sendStatus(201);
-    }).catch(error => {
-        console.log('error on server in POST:', error);
-        res.sendStatus(500);
-    })
+        .then(result => {
+            res.sendStatus(201);
+        }).catch(error => {
+            console.log('error on server in POST:', error);
+            res.sendStatus(500);
+        })
 });
+
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+    console.log('in DELETE, req.params is:', req.params);
+    console.log('in DELETE, req.body is:', req.body);
+
+    const queryText = `DELETE FROM "wine" WHERE "id" = $1;`;
+    
+    pool.query(queryText, [req.params.id])
+        .then(response => {
+            res.sendStatus(200);
+        }).catch(error => {
+            console.log('error on server in DELETE:', error)
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router  
