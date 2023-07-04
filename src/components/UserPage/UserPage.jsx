ba25b2fa77800e96f5771a7a8c18db43b3511385
Adam Oberle <oberle.adam@gmail.com>
2023-07-04 18:33:03 -0500

    started user profile page before realizing still on main branch

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,24 +3,32 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom"
+
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
-//   const dispatch = useDispatch();
-  
-//   useEffect(() => {
-//     dispatch({ type: 'FETCH_WINE'})
-// }, [])
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+}, [])
 
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
+      <h1 className='centering'>Welcome, {user.username}!</h1>
+      <p>Press the Go To Cellar Button to check out your collection, or press the Add Wine button
+        to get started adding a bottle of wine to your collection!
+      </p>
+      <br/>
+      <p>You have {} bottles of wine in your collection!</p>
+      <br/>
+      <p>You have rated {} bottles of wine in your collection, nice job!</p>
+      <br/>
+      
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="premade_btn" />
     </div>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
